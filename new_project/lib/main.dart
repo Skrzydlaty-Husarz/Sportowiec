3e3dee811f6b98a8210b38b5f04da337bdddab3b
DESKTOP-LJIN95K\Pc <sharkyboy202@gmail.com>
2021-03-19 15:55:29 +0100

    Szczęsny 2.0

--- new_project/lib/main.dart
+++ new_project/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(home: FootballPlayer(),)
   );
 }
@@ -14,117 +14,133 @@ class FootballPlayer extends StatelessWidget {
       body: Container(
           width: double.infinity,
           child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
 
-                  child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                    children:[
+                child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
                       CircleAvatar(
-                      radius: 60,
-                      backgroundImage: NetworkImage(
-                        "https://pbs.twimg.com/profile_images/891114812796796928/PVjctqFC.jpg"),
+                        radius: 60,
+                        backgroundImage: NetworkImage(
+                            "https://pbs.twimg.com/profile_images/891114812796796928/PVjctqFC.jpg"),
 
-                ),
-                  ]
+                      ),
+                    ]
                 ),
               ),
 
 
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 16),
-                  child: Text(
-                    "Wojciech Szczęsny",
-                    style: TextStyle(fontSize: 22),
-                  ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 16),
+                child: Text(
+                  "Wojciech Szczęsny",
+                  style: TextStyle(fontSize: 22),
                 ),
+              ),
 
-                Container(
-                  margin: EdgeInsets.only(left: 16),
-                  child: Text(
-                    "Polski piłkarz występujący na pozycji bramkarza we włoskim klubie"
+              Container(
+                margin: EdgeInsets.only(left: 16),
+                child: Text(
+                  "Polski piłkarz występujący na pozycji bramkarza we włoskim klubie"
                       "Juventus F.C. oraz w reprezentacji Polski. Uczestnik Mistrzostw Świata 2018, Mistrzostw Europy 2012 i 2016. Syn Macieja Szczęsnego.",
                   style: TextStyle(color: Colors.blue),
                   textAlign: TextAlign.center,
-                  ),
                 ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-                  child:  Row(
-                      children: [
-                        Expanded(
-                          child: Text(
-                            "Moja ocena:",
-                          ),
-                        ),
-                        Icon(Icons.star),
-                        Icon(Icons.star),
-                        Icon(Icons.star),
-                        Icon(Icons.star),
-                        Icon(Icons.star_border),
-                      ],
-                  )
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "Moja ocena:",
+                      ),
 
+                    ),
+                    StarsRow(3),
+                  ],
                 ),
-
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-                  padding: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(8),
-
-                  ),
-                  child: Row(crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children:[
-                        Icon(Icons.cake),
-                        Text('18.04.1990'),
-
-                      ]
-                  ),
-                  ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-                  padding: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(8),
-
-                  ),
-                  child: Row(crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children:[
-                        Icon(Icons.sports_soccer),
-                        Text('Juventys Turyn'),
-
-                      ]
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-                  padding: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(8),
-
-                  ),
-                  child: Row(crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children:[
-                        Icon(Icons.arrow_upward),
-                        Text('1.95'),
-
-                      ]
-                  ),
-                ),
-
-              ]
+              ),
+              LeBox(Icons.cake, '18.04.1990'),
+              LeBox(Icons.sports_soccer, 'Juventys Turyn'),
+              LeBox(Icons.arrow_upward, '1.95')
+            ],
           )
+
+
       ),
     );
   }
 }
+
+
+class StarsRow extends StatelessWidget {
+  final int rating;
+
+  const StarsRow(this.rating, {
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> stars = [];
+    for (int i = 0; i < 4; i++) {
+      if (i < rating) {
+        stars.add(Icon(Icons.star));
+      }
+      else {
+        stars.add(Icon(Icons.star_border));
+      }
+    }
+    return Row(
+        children:
+        stars
+    );
+  }
+}
+
+class LeBox extends StatelessWidget {
+  final IconData icon;
+  final String dum;
+
+  const LeBox(this.icon,
+      this.dum, {
+        Key key,
+      }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> box = [];
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+      padding: EdgeInsets.all(8),
+      child: Row(
+        children: [
+          Icon(icon),
+          Expanded(
+            child: Text(
+              (dum),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+      ),
+    );
+    box.add(Icon(icon));
+    box.add(Text(dum));
+    return Container(
+      child: Row(
+        children: box,
+      ),
+    );
+  }
+}
+
